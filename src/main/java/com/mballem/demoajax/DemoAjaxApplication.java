c7f1b051628fa19ac9fbdc814c2888ff7c6ee946
Jairo <jnsousa@stefanini.com>
2019-06-11 10:19:01 -0300

    Capturando o conteudo via OpenGraph

--- src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -1,13 +1,30 @@
 package com.mballem.demoajax;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mballem.demoajax.web.controller.domain.SocialMetaTag;
+import com.mballem.demoajax.web.service.SocialMetaTagService;
+
 @SpringBootApplication
-public class DemoAjaxApplication {
+public class DemoAjaxApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAjaxApplication.class, args);
+	}
+	
+	@Autowired
+	private SocialMetaTagService service;
+
+	@Override
+	public void run(String... args) throws Exception {
+		 
+		SocialMetaTag og = service.getOpenGraphByUrl("https://www.udemy.com/spring-boot-mvc-com-thymeleaf/");
+		
+		System.out.println(og.toString());
+		
 	}
 
 }
