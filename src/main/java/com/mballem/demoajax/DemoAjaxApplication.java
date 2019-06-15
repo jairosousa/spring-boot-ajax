9e90f24507fbd93f59f53d77ebf9753638b453df
jaironsousa1 <jaironsousa@gmail.com>
2019-06-15 08:33:55 -0300

    10 - Capturando o conteudo via TwitterCard

--- src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mballem.demoajax.web.controller.domain.SocialMetaTag;
-import com.mballem.demoajax.web.service.SocialMetaTagService;
+import com.mballem.demoajax.domain.SocialMetaTag;
+import com.mballem.demoajax.service.SocialMetaTagService;
 
 @SpringBootApplication
 public class DemoAjaxApplication implements CommandLineRunner{
@@ -21,10 +21,16 @@ public class DemoAjaxApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		 
-		SocialMetaTag og = service.getOpenGraphByUrl("https://www.udemy.com/spring-boot-mvc-com-thymeleaf/");
+		SocialMetaTag og = service.getOpenGraphByUrl("https://www.pichau.com.br/placa-mae-asus-prime-b450m-a-ddr4-socket-am4-chipset-amd-b450");
 		
 		System.out.println(og.toString());
-		
+
+		System.out.println("---------------------------------------------------");
+
+		SocialMetaTag twittwr = service.getTwitterCardByUrl("https://www.udemy.com/spring-boot-mvc-com-thymeleaf/");
+
+		System.out.println(twittwr.toString());
+
 	}
 
 }
---
+++ src/main/java/com/mballem/demoajax/service/SocialMetaTagService.java
@@ -0,0 +1,54 @@
+package com.mballem.demoajax.service;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.mballem.demoajax.domain.SocialMetaTag;
+
+@Service
+public class SocialMetaTagService {
+
+	private static Logger log = LoggerFactory.getLogger(SocialMetaTagService.class);
+	
+	public SocialMetaTag getTwitterCardByUrl(String url) {
+
+		SocialMetaTag tag = new SocialMetaTag();
+
+		try {
+			Document doc = Jsoup.connect(url).get();
+			tag.setTitle(doc.head().select("meta[name=twitter:title]").attr("content"));
+			tag.setSite(doc.head().select("meta[name=twitter:site]").attr("content"));
+			tag.setImage(doc.head().select("meta[name=twitter:image]").attr("content"));
+			tag.setUrl(doc.head().select("meta[name=twitter:url]").attr("content"));
+		} catch (IOException e) {
+			log.error(e.getMessage(), e.getCause());
+		}
+
+		return tag;
+
+	}
+
+	public SocialMetaTag getOpenGraphByUrl(String url) {
+
+		SocialMetaTag tag = new SocialMetaTag();
+
+		try {
+			Document doc = Jsoup.connect(url).get();
+			tag.setTitle(doc.head().select("meta[property=og:title]").attr("content"));
+			tag.setSite(doc.head().select("meta[property=og:site_name]").attr("content"));
+			tag.setImage(doc.head().select("meta[property=og:image]").attr("content"));
+			tag.setUrl(doc.head().select("meta[property=og:url]").attr("content"));
+		} catch (IOException e) {
+			log.error(e.getMessage(), e.getCause());
+		}
+
+		return tag;
+
+	}
+
+}
