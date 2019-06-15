682d89735803e8687b3d262f6ba0f75ca8baaee3
jaironsousa1 <jaironsousa@gmail.com>
2019-06-15 14:59:12 -0300

    17 - Capturando status error 404 e 500

--- src/main/resources/static/js/promo-add.js
+++ src/main/resources/static/js/promo-add.js
@@ -4,15 +4,24 @@ $("#linkPromocao").on('change', function () {
 
     if (url.length > 7) {
         $.ajax({
-            method:"POST",
-            url:"/meta/info?url=" + url,
+            method: "POST",
+            url: "/meta/info?url=" + url,
             cache: false,
-            success: function ( data ) {
+            success: function (data) {
                 // console.log(data);
                 $("#titulo").val(data.title);
-                $("#site").text(data.site.replace("@",""));
+                $("#site").text(data.site.replace("@", ""));
                 $("#linkImagem").attr("src", data.image);
+            },
+            statusCode: {
+                404: function () {
+                    $("#alert").addClass("alert alert-danger").text("Nenhuma informação pode ser recuperada dessa url.");
+                }
+
+            },
+            error: function (err) {
+                $("#alert").addClass("alert alert-danger").text("Ops... algo deu errado tente maais tarde");
             }
-        })
+        });
     }
-})
+});
