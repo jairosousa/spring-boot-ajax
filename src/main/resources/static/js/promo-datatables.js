4f3aa0d6269e3f85bb4abc0b637f9a2491995a0c
jaironsousa1 <jaironsousa@gmail.com>
2019-06-17 20:16:53 -0300

    71 - A requisição ajax para update.

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -64,6 +64,46 @@ $(document).ready(function () {
         }
     });
 
+    // submit do formulario para editar
+    $("#btn-edit-modal").on("click", function () {
+        var promo = {};
+        promo.titulo = $("#edt_titulo").val();
+        promo.descricao = $("edt_#descricao").val();
+        promo.preco = $("#edt_preco").val();
+        promo.categoria = $("#edt_categoria").val();
+        promo.linkImagem = $("#edt_linkImagem").val();
+        promo.id = $("#edt_id").val();
+
+        $.ajax({
+            method: "POST",
+            url: "/promocao/edit/",
+            data: promo,
+            success: function () {
+                $("#modal-form").modal('hide');
+                table.ajax.reload();
+            },
+            statusCode: {
+                //Mostrar mensagem de erro de validação
+                422: function (xhr) {
+                    // console.log(xhr.status);
+                    var errors = $.parseJSON(xhr.responseText);
+                    $.each(errors, function (key, val) {
+                        $("#edt_" + key).addClass("is-invalid"); //Borda avermelhada
+                        $("#error-" + key)
+                            .addClass("invalid-feedback")
+                            .append("<span class='error-span'>" + val + "</span>");
+                    });
+                }
+            }
+        });
+    });
+
+    // Alterar a imagem no componente <img> modal
+    $("#edt_linkImagem").on("change", function () {
+       var link = $(this).val();
+       $("#edt_imagem").attr("src", link);
+    });
+
     // Recupera as informações para pre-editar
     $("#btn-editar").on("click", function () {
         if (isSelectedRow()) {
@@ -104,7 +144,6 @@ $(document).ready(function () {
 
     function getPromoId() {
         return table.row(table.$('tr.selected')).data().id;
-        ;
     }
 
     function isSelectedRow() {
