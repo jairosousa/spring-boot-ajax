7fd81ac2081b724ad553e8ca0733a513619caa11
jaironsousa1 <jaironsousa@gmail.com>
2019-06-18 19:47:43 -0300

    75. Integrando a DWR ao Spring

--- src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
+++ src/main/java/com/mballem/demoajax/DemoAjaxApplication.java
@@ -1,23 +1,37 @@
 package com.mballem.demoajax;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
-import com.mballem.demoajax.domain.SocialMetaTag;
-import com.mballem.demoajax.service.SocialMetaTagService;
-
+@ImportResource(locations = "classpaht:drw-spring.xml")
 @SpringBootApplication
-public class DemoAjaxApplication implements CommandLineRunner{
+public class DemoAjaxApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoAjaxApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		 
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAjaxApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+    @Bean
+    public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet() {
+        DwrSpringServlet dwrServlet = new DwrSpringServlet();
+
+        ServletRegistrationBean<DwrSpringServlet> registrationBean =
+                new ServletRegistrationBean<>(dwrServlet, "/dwr/*");
+
+        registrationBean.addInitParameter("debug", "true");
+        registrationBean.addInitParameter("activeReverseAjaxEnabled", "true");
+
+        return registrationBean;
+    }
 
 }
